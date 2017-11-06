4fdfd8c994b100639d59e4ba04c06dfa63530aae
Unknown <taekwonaa@gmail.com>
2017-11-05 22:13:40 -0500

    Project Creation
    Created the Java Project

---
+++ WorkoutKeeper/src/WorkoutKeeper.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+
+/**
+ * @author Aleksandar
+ *
+ */
+public class WorkoutKeeper {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World");
+	}
+
+}
