3c562f452e1150445b4ab002b40d74f6e99ff6b4
Unknown <taekwonaa@gmail.com>
2017-11-05 22:37:11 -0500

    Pull Test
    Test pulling

--- WorkoutKeeper/src/WorkoutKeeper.java
+++ WorkoutKeeper/src/WorkoutKeeper.java
@@ -14,8 +14,11 @@ public class WorkoutKeeper {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello World");
-      
-      System.out.println("Hello, Alex, from the other side.");
+
+		System.out.println("Hello, Alex, from the other side.");
+
+		System.out.println("Pull Testing");
+
 	}
 
 }
