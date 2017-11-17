6e9b81110ed92d6e060116bff1025a77015cca96
Unknown <taekwonaa@gmail.com>
2017-11-16 21:47:27 -0500

    Created Welcome Box, Create Account, and Login Box
    Made the first box that will appear after program opens and the Login Box the user can navigate to.

---
+++ WorkoutKeeper/src/Validate.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+
+/**
+ * @author Aleksandar
+ *
+ */
+public class Validate {
+
+	public static boolean validateEmail(String email) {
+		// TODO Auto-generated method stub
+		return true;
+	}
+
+	public static boolean validatePassword(String password) {
+		// TODO Auto-generated method stub
+		return true;
+	}
+}
