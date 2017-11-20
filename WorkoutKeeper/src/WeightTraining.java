f74b42d2299a24ce03fea9c43ac62f74bb2a99ec
Unknown <xangres@gmail.com>
2017-11-19 19:09:53 -0500

    Two new DDCs.
    WeightTraining and Workout.

---
+++ WorkoutKeeper/src/WeightTraining.java
@@ -0,0 +1,66 @@
+/* */
+
+public class WeightTraining extends Exercise {
+	//the weight required for the exercise equipment in pounds (lbs)
+	private int weight;
+	//the number of repetitions the weight will be lifted
+	private int reps;
+	
+	/* 
+	 * constructor that sets the ID and increments the exercise counter
+	 * */
+	public WeightTraining() {
+		super();
+	}
+
+	/**
+	 * @return the weight
+	 */
+	public double getWeight() {
+		return weight;
+	}
+
+	/**
+	 * Validates and sets the weight required for the exercise equipment
+	 * @param weight the weight to set
+	 * @return boolean true indicating that setting weight was successful
+	 * @throws IllegalArgumentException
+	 */
+	public boolean setWeight(int weight) throws IllegalArgumentException {
+		if(weight <= 0) {
+			throw new IllegalArgumentException("Error: not a valid weight for training equipment!");
+		}
+		else {
+			this.weight = weight;
+			return true;
+		}
+	}
+
+	/**
+	 * @return the reps
+	 */
+	public int getReps() {
+		return reps;
+	}
+
+	/**
+	 * Validates and sets the number of repetitions the weight will be lifted
+	 * @param reps the reps to set
+	 * @return boolean true indicating that setting reps was successful
+	 * @throws IllegalArgumentException
+	 */
+	public boolean setReps(int reps) throws IllegalArgumentException {
+		if(reps <= 0) {
+			throw new IllegalArgumentException("Error: not a valid number of repetitions!");
+		}
+		else {
+			this.reps = reps;
+			return true;
+		}
+	}
+	
+	/* Prepares a record of the weight training exercise's information and returns it */
+	public String toString() {
+		return("Type: " + this.getClass().getName() + ", " + super.toString() + ", Weight: " + this.weight + ", Reps: " + this.reps);
+	}
+}
---
+++ WorkoutKeeper/src/Workout.java
@@ -0,0 +1,60 @@
+/* */
+
+public class Workout {
+	//system-generated ID to track workouts
+	private static int workoutID;
+	//the workout's own unique ID
+	private int ID;
+	//can never be partially filled, so does not need a counter
+	private Exercise[] workout;
+	//constants indicating the exact number of each exercise type needed in each workout
+	public static final int MAX_CARDIO = 1;
+	public static final int MAX_STRETCH = 3;
+	public static final int MAX_WEIGHT = 5;
+	
+	/*constructor assigns the newest ID and increments the workout counter */
+	public Workout() {
+		this.ID = workoutID;
+		workoutID++;
+		this.workout = new Exercise[MAX_CARDIO + MAX_STRETCH + MAX_WEIGHT];
+	}
+	
+	public int getWorkoutID() {
+		return workoutID;
+	}
+	
+	public int getID() {
+		return ID;
+	}
+	
+	/* Returns a copy of the array of exercises constituting the workout
+	 * @return copy A copy of the array of exercises constituting the workout */
+	public Exercise[] getWorkout() {
+		//copy the array and its elements
+		Exercise[] copy = new Exercise[this.workout.length];
+		for(int i = 0; i < this.workout.length; i++) {
+			copy[i] = this.workout[i];
+		}
+		return copy;
+	}
+	
+	/* Copies over the contents of inputted array of exercises
+	 * @param workout The inputted array of exercises to create a workout using */
+	public void setWorkout(Exercise[] workout) {
+		for(int i = 0; i < this.workout.length; i++) {
+			this.workout[i] = workout[i];
+		}
+	}
+	
+	/* Prepares a record for the workout */
+	public String toString() {
+		String list = "";
+		for(int i = 0; i < this.workout.length; i++) {
+			list += this.workout[i];
+			if(i < this.workout.length-1) {
+				list += ", ";
+			}
+		}
+		return ("ID: " + this.ID + ", Workouts: " + list);
+	}
+}
